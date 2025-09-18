4b4170cfa07e6c92d29dc0cc74fd73ebeee5560d
Carina Huang <carinahuang06@gmail.com>
2025-09-17 17:52:14 -0700

    first commitit

---
+++ game.java
@@ -0,0 +1,7 @@
+//initial commit
+public class game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Black Jack");
+        // Prompt for player's name
+    }
+}
---
+++ menu.java
@@ -0,0 +1,6 @@
+public class menu {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Black Jack Menu");
+        // Display menu options
+    }
+}
